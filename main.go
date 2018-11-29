5eab21a10b33d73cfc1bb3c0f841347980105ab7
Kyle Robertson <jsakyle@gmail.com>
2018-11-29 15:55:42 -0500

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "What up fam squad"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
